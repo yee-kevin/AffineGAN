978f248a8d77ed9682cee91c7489eb58e72c8d44
Kevin <37497875+yee-kevin@users.noreply.github.com>
2020-07-08 19:28:01 +0800

    Update frames_to_gif
    Generates gif from frames with a filepath

--- frames_to_gif.py
+++ frames_to_gif.py
@@ -1,10 +1,10 @@
 import imageio
 import glob
 
-filepath = "./UTDAL_04651v113_generated" + "/*"
+filepath = "./dataset/UTDAL_smile/generated/UTDAL_04651v113" + "/*"
 filenames = glob.glob(filepath)
 
-with imageio.get_writer('./UTDAL_04651v113_generated.gif', mode='I') as writer:
+with imageio.get_writer('./GIFS/UTDAL_04651v113_generated.gif', mode='I') as writer:
     for filename in filenames:
         image = imageio.imread(filename)
         writer.append_data(image)
